48b95017a2998d0cb049f0c5dcb5ae6de0d34e18
Magnus Edenhill <magnus@edenhill.se>
2014-06-26 21:49:27 +0200

    mklove update for STATIC_LIB_..

--- mklove/modules/configure.cc
+++ mklove/modules/configure.cc
@@ -2,7 +2,7 @@
 #
 # Compiler detection
 # Sets:
-#  CC, CXX, CFLAGS, CPPFLAGS, LDFLAGS, PKG_CONFIG, instALL
+#  CC, CXX, CFLAGS, CPPFLAGS, LDFLAGS, ARFLAGS, PKG_CONFIG, INSTALL, MBITS
 
 
 mkl_require host
@@ -42,6 +42,19 @@ function checks {
         mkl_mkvar_set "CXX" CXX $CXX
     fi
 
+    # Handle machine bits, if specified.
+    if [[ ! -z "$MBITS" ]]; then
+	mkl_meta_set mbits_m name "mbits compiler flag (-m$MBITS)"
+	if mkl_compile_check mbits_m "" fail CC "-m$MBITS"; then
+	    mkl_mkvar_append CPPFLAGS CPPFLAGS "-m$MBITS"
+	    mkl_mkvar_append LDFLAGS LDFLAGS "-m$MBITS"
+	fi
+	if [[ -z "$ARFLAGS" && $MBITS == 64 && $MKL_DISTRO == "SunOS" ]]; then
+	    # Turn on 64-bit archives on SunOS
+	    mkl_mkvar_append ARFLAGS ARFLAGS "S"
+	fi
+    fi
+
     # Provide prefix and checks for various other build tools.
     local t=
     for t in LD:ld NM:nm OBJDUMP:objdump STRIP:strip ; do
@@ -60,12 +73,25 @@ function checks {
     done
 
     # Compiler and linker flags
-    [[ ! -z $CFLAGS ]]   && mkl_mkvar_append "CFLAGS" "CFLAGS" $CFLAGS
-    [[ ! -z $CPPFLAGS ]] && mkl_mkvar_append "CPPFLAGS" "CPPFLAGS" $CPPFLAGS
-    [[ ! -z $CXXFLAGS ]] && mkl_mkvar_append "CFLAGS" "CXXFLAGS" $CXXFLAGS
-    [[ ! -z $LDFLAGS ]]  && mkl_mkvar_append "CFLAGS" "LDFLAGS" $LDFLAGS
+    [[ ! -z $CFLAGS ]]   && mkl_mkvar_set "CFLAGS" "CFLAGS" "$CFLAGS"
+    [[ ! -z $CPPFLAGS ]] && mkl_mkvar_set "CPPFLAGS" "CPPFLAGS" "$CPPFLAGS"
+    [[ ! -z $CXXFLAGS ]] && mkl_mkvar_set "CXXFLAGS" "CXXFLAGS" "$CXXFLAGS"
+    [[ ! -z $LDFLAGS ]]  && mkl_mkvar_set "LDFLAGS" "LDFLAGS" "$LDFLAGS"
+    [[ ! -z $ARFLAGS ]]  && mkl_mkvar_set "ARFLAGS" "ARFLAGS" "$ARFLAGS"
 
-    mkl_mkvar_append CPPFLAGS CPPFLAGS "-g"
+
+    # Add debug symbol flag (-g)
+    # OSX 10.9 requires -gstrict-dwarf for some reason.
+    mkl_meta_set cc_g_dwarf name "debug symbols compiler flag (-g...)"
+    if [[ $MKL_DISTRO == "osx" ]]; then
+	if mkl_compile_check cc_g_dwarf "" cont CC "-gstrict-dwarf"; then
+	    mkl_mkvar_append CPPFLAGS CPPFLAGS "-gstrict-dwarf"
+	else
+	    mkl_mkvar_append CPPFLAGS CPPFLAGS "-g"
+	fi
+    else
+	mkl_mkvar_append CPPFLAGS CPPFLAGS "-g"
+    fi
 
 
     # pkg-config
@@ -82,7 +108,16 @@ function checks {
 
     # install
     if [ -z "$INSTALL" ]; then
-        INSTALL=install
+	if [[ $MKL_DISTRO == "SunOS" ]]; then
+	    mkl_meta_set ginstall name "GNU install"
+	    if mkl_command_check ginstall "" ignore "ginstall --version"; then
+		INSTALL=ginstall
+	    else
+		INSTALL=install
+	    fi
+        else
+            INSTALL=install
+	fi
     fi
 
     if mkl_command_check "install" "WITH_INSTALL" cont "$INSTALL --version"; then
@@ -120,9 +155,9 @@ mkl_option "Compiler" "env:CC" "--cc=CC" "Build using C compiler CC" "\$CC"
 mkl_option "Compiler" "env:CXX" "--cxx=CXX" "Build using C++ compiler CXX" "\$CXX"
 mkl_option "Compiler" "ARCH" "--arch=ARCH" "Build for architecture" "$(uname -m)"
 mkl_option "Compiler" "CPU" "--cpu=CPU" "Build and optimize for specific CPU" "generic"
+mkl_option "Compiler" "MBITS" "--mbits=BITS" "Machine bits (32 or 64)" ""
 
-
-for n in CFLAGS CPPFLAGS CXXFLAGS LDFLAGS; do
+for n in CFLAGS CPPFLAGS CXXFLAGS LDFLAGS ARFLAGS; do
     mkl_option "Compiler" "mk:$n" "--$n=$n" "Add $n flags"
 done
 
