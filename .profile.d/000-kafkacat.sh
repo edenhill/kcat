4aea14aa8b15a6afddfda9e3b8f854b41dbb4118
Trevor Scott <tscott@heroku.com>
2017-09-14 16:14:31 -0700

    prefix kafkacat with ssl config

--- .profile.d/000-kafkacat.sh
+++ .profile.d/000-kafkacat.sh
@@ -1,1 +1,10 @@
-export PATH="/app/kafkacat/bin:$PATH"
+export KAFKACAT_HOME="/app/kafkacat"
+
+# write kafka ssl config vars to files
+mkdir -p $KAFKACAT_HOME/env
+
+echo "$KAFKA_TRUSTED_CERT" > $KAFKACAT_HOME/env/KAFKA_TRUSTED_CERT
+echo "$KAFKA_CLIENT_CERT" > $KAFKACAT_HOME/env/KAFKA_CLIENT_CERT
+echo "$KAFKA_CLIENT_CERT_KEY" > $KAFKACAT_HOME/env/KAFKA_CLIENT_CERT_KEY
+
+export PATH="/app/bin:$PATH"
